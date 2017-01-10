01ccd7cc9c54d516001048ff139c49de8b2ec82b
MTTHWBSH <bushmat4@gmail.com>
2017-01-10 15:25:45 -0600

    initial commit

---
+++ Growler/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Growler
+//
+//  Created by Matt Bush on 1/10/17.
+//  Copyright © 2017 Growler. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        // Config
+        
+        return true
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Growler/Pods-Growler-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Growler : NSObject
+@end
+@implementation PodsDummy_Pods_Growler
+@end
