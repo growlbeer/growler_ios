acf8c5b19e0f735716a96c03e32cf398d2995e8c
MTTHWBSH <bushmat4@gmail.com>
2017-01-10 16:03:45 -0600

    config

---
+++ Growler/Utilities/Configuration.swift
@@ -0,0 +1,11 @@
+//
+//  Configuration.swift
+//  Growler
+//
+//  Created by Matt Bush on 1/10/17.
+//  Copyright © 2017 Growler. All rights reserved.
+//
+
+public final class Configuration {
+    
+}
