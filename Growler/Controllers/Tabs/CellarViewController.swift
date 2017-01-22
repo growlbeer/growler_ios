56b91ab7d5b89e94289cc50438a0ed009b01922d
Mike Cruz <michaeldcruz@gmail.com>
2017-01-21 21:18:16 -0600

    I'm Ron Burgundy\?

--- Growler/Controllers/Tabs/CellarViewController.swift
+++ Growler/Controllers/Tabs/CellarViewController.swift
@@ -13,10 +13,14 @@ class CellarViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+        QueryClient.sharedClient.fetch(query: BeersQuery()) { (result, error) in
+            guard let data = result?.data else { return }
+            print(data.beers)
+        }
     }
-    
+
     static func title() -> String { return "Cellar" }
-    
+
     fileprivate func setupView() {
         title = CellarViewController.title()
         view.backgroundColor = UIColor.orange
--- Growler/Utilities/Constants.swift
+++ Growler/Utilities/Constants.swift
@@ -9,5 +9,5 @@
 import Foundation
 
 struct Constants {
-    static let kQueryURL = "http://api.growl.beer/query"
+    static let kQueryURL = "http://api.growl.beer/queries"
 }
